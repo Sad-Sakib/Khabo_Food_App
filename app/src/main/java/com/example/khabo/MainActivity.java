460369e09bbfe4fe595e20f3eb372919d60d3b5e
Sadman Sakib <natrium213@gmail.com>
2022-09-16 17:34:23 +0600

    food list is added

--- app/src/main/java/com/example/khabo/MainActivity.java
+++ app/src/main/java/com/example/khabo/MainActivity.java
@@ -6,11 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     Button login;
     Button reg;
+    TextView skip;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,5 +41,17 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+        skip=findViewById(R.id.Skip);
+        skip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(MainActivity.this, "Food List!", Toast.LENGTH_SHORT).show();
+                Intent intent= new Intent(MainActivity.this,Food_list.class);
+                startActivity(intent);
+
+            }
+        });
+
     }
 }
